484e7d1f7298fc246e387890f3384555fa90d052
Abigail Doyle <abigaildoyle3@gmail.com>
2024-12-23 17:12:30 -0500

    add loader gif

--- src/app/smart/components/countdown-clock/countdown-clock.component.ts
+++ src/app/smart/components/countdown-clock/countdown-clock.component.ts
@@ -1,17 +1,18 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core'
 import { interval } from 'rxjs'
 import { CountdownComponentComponent } from '../../../ui/components/countdown-component/countdown-component.component'
-import { isPlatformBrowser } from '@angular/common'
+import { CommonModule, isPlatformBrowser } from '@angular/common'
 
 @Component({
   selector: 'app-countdown-clock',
   standalone: true,
-  imports: [CountdownComponentComponent],
+  imports: [CountdownComponentComponent, CommonModule],
   templateUrl: './countdown-clock.component.html',
   styleUrls: ['./countdown-clock.component.scss'],
 })
 export class CountdownClockComponent implements OnInit {
   timeLeft = { days: 0, hours: 0, minutes: 0, seconds: 0 }
+  isLoading = true
 
   private targetDate = new Date('2026-07-04T00:00:00') // Replace with your target date
   private browserOnly: boolean
@@ -22,13 +23,16 @@ export class CountdownClockComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.browserOnly) {
+      this.updateCountdown()
+      setTimeout(() => {
+        this.isLoading = false // Stop the loader after a delay
+      }, 5000) // 2 seconds delay
       interval(1000).subscribe(() => this.updateCountdown())
     }
   }
 
   private updateCountdown(): void {
     const now = new Date().getTime()
-
     const difference = this.targetDate.getTime() - now
 
     if (difference > 0) {
